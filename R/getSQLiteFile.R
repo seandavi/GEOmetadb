139b754d6caeb9f7179e4d99ec9ad751db5d3260
Sean Davis <sdavis2@mail.nih.gov>
2009-05-14 11:09:18 +0000

    Changed a couple of print statements in getSQLiteFile() to cat() statements instead--just cosmetic.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOmetadb@39449 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getSQLiteFile.R
+++ R/getSQLiteFile.R
@@ -4,14 +4,14 @@ function(destdir=getwd(),destfile='GEOmetadb.sqlite.gz') {
   # download.file('http://meltzerlab.nci.nih.gov/apps/geo/GEOmetadb.sqlite.gz', destfile=localfile,mode='wb')
   download.file('http://gbnci.abcc.ncifcrf.gov/geo/GEOmetadb.sqlite.gz', destfile=localfile,mode='wb')
   require(GEOquery) ### for gunzip
-  print('Unzipping...')
+  cat('Unzipping...\n')
   gunzip(localfile,overwrite=TRUE)
   unzippedlocalfile <- gsub('[.]gz$','',localfile)
   require(RSQLite)
   con <- dbConnect(SQLite(),unzippedlocalfile)
   dat <- dbGetQuery(con,'select * from metaInfo')
   dbDisconnect(con)
-  print("Metadata associate with downloaded file")
+  cat("Metadata associate with downloaded file:\n")
   print(dat)
   return(unzippedlocalfile)
 }
