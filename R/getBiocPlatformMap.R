d0d5a5aceed7eee1649cfda9860c22968598f5c3
Jack Zhu <zhujack@mail.nih.gov>
2011-02-04 02:53:53 +0000

    Modified the condition
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOmetadb@52443 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getBiocPlatformMap.R
+++ R/getBiocPlatformMap.R
@@ -3,15 +3,17 @@
 ## query and get mappings betwen GPL accessions and bioc_package names
 
 ## Usage of getBiocPlatformMap:
-## arguments: getBiocPlatformMap (con)
+## arguments: getBiocPlatformMap (con, bioc='all')
 
 ################################################################################
+
+
 
 getBiocPlatformMap <-
 function(con, bioc='all') {
 	
 	bioc=tolower(bioc)
-	if(bioc == 'all') {
+	if( any(bioc %in% 'all')) {
 		biocmap = dbGetQuery(con,'select title,gpl,bioc_package,manufacturer,organism,data_row_count from gpl where bioc_package is not NULL ORDER by bioc_package')
 	} else {
 		biocmap = dbGetQuery(con, paste("select title,gpl,bioc_package,manufacturer,organism,data_row_count from gpl where bioc_package IN ('", paste(bioc, sep='', collapse="','"), "') AND bioc_package IS NOT NULL ORDER by bioc_package", sep=''))
