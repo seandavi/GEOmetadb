1ab9e2fcba36a6345590de61c280bb65b97421e1
Jack Zhu <zhujack@mail.nih.gov>
2011-12-01 03:17:21 +0000

    Added Dropbox URL as an alternative downloading source
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOmetadb@61006 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getSQLiteFile.R
+++ R/getSQLiteFile.R
@@ -1,11 +1,18 @@
-`getSQLiteFile` <-
+getSQLiteFile <-
 function(destdir=getwd(),destfile='GEOmetadb.sqlite.gz') {
   localfile <- file.path(destdir,destfile)
-  ERR <- try( download.file('http://gbnci.abcc.ncifcrf.gov/geo/GEOmetadb.sqlite.gz', destfile=localfile,mode='wb'), silent=T )
-  ##alternative download site
-  if( inherits(ERR, "try-error") ) {
-    download.file('http://watson.nci.nih.gov/~zhujack/GEOmetadb.sqlite.gz', destfile=localfile,mode='wb')
+  url_geo_1 = 'http://gbnci.abcc.ncifcrf.gov/backup/GEOmetadb.sqlite.gz'
+  url_geo_2 = 'http://watson.nci.nih.gov/~zhujack/GEOmetadb.sqlite.gz'
+  url_geo_3 = 'http://dl.dropbox.com/u/51653511/GEOmetadb.sqlite.gz'
+  if(! inherits(try(url(url_geo_1, open='rb'), silent = TRUE), "try-error") ) {
+     url_geo = url_geo_1
+  } else if(! inherits(try(url(url_geo_2, open='rb'), silent = TRUE), "try-error") ) {
+     url_geo = url_geo_2
+  } else {
+     url_geo = url_geo_3
   }
+
+  download.file(url_geo, destfile=localfile,mode='wb')
   require(GEOquery) ### for gunzip
   cat('Unzipping...\n')
   gunzip(localfile,overwrite=TRUE)
